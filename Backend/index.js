1782377d96ca590d872f64831595ed3c0e70d9a2
himani bhimate <himanibhimate0808@gmail.com>
2024-07-02 23:37:55 +0530

    add middleware

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app =  express();
 const cors = require("cors")
 const bodyparser = require("body-parser")
+
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL)
 
