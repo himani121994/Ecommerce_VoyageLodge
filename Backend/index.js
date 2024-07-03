7e3971b78e4d6c58090d5de0b39e3a2104d0645e
himani bhimate <himanibhimate0808@gmail.com>
2024-07-03 10:58:18 +0530

    env setup

--- Backend/index.js
+++ Backend/index.js
@@ -4,14 +4,16 @@ const app =  express();
 const cors = require("cors")
 const bodyparser = require("body-parser")
 
+
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL)
-
 
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cors());
+
+
 
 
 app.listen(8000,()=>{
