e9f44e93f129714edec95ecc72aedf0e49ada114
himani bhimate <himanibhimate0808@gmail.com>
2024-07-17 14:45:57 +0530

    first commit

--- voyagelodge/src/App.jsx
+++ voyagelodge/src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
 import axios from "axios"
 import './App.css'
+import Home from './Component/home'
 
 const App =()=>{
-  const [input,setInput] = useState({})
-
-   const submitedata =()=>{
-     let url = "http://localhost:8000/admin";
-     axios.post(url,input).then((res)=>{
-      setInput(res.data)
-     })
-   }
   return(
     <>
-    <form onSubmit={submitedata}>
-      <input type="text" name="input" onChange={e=>setInput(e.target.value)} />
-      <button>click</button>
-    </form>
+    <Home/>
     </>
   )
 }
